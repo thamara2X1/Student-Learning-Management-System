8775926e8aa8016ca9d977d1dbd971b4defa79c1
thamara2X1 <vininduvtph@gmail.com>
2025-09-22 19:30:11 +0530

    1st commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "lms-project-7a9fd", appId: "1:261397624469:web:7690a116b999d9bd214d9c", storageBucket: "lms-project-7a9fd.firebasestorage.app", apiKey: "AIzaSyDT3giGUL2MpTQeC6MQDsILvpu0E8Nnig8", authDomain: "lms-project-7a9fd.firebaseapp.com", messagingSenderId: "261397624469", measurementId: "G-ELDSY0ELG8" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage())
   ]
 };
